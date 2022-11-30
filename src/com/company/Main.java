38d561ad1eb249f34b356f6183cea9785fa4febf
Nitish Kumar Tiwari <94640716+Nitishtiwari17@users.noreply.github.com>
2022-11-30 23:38:03 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args)throws java.lang.Exception
+    {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                int N = n1 * 5 + n2 * 7;
+                System.out.println(N);
+            }
+        }
+    }
+
+
+
+
+
